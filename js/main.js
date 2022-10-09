e1fb4350fa564fee7a2385a65eab0317e42c6997
srobertson77 <sirbralin.robertson@protonmail.com>
2022-10-09 06:31:46 -0500

    App section complete

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
-// Call Hello World
-function HelloWorld() {
-    alert("Hello World");
+//Get the values of the string
+//Controller function
+function getValue()
+{
+    document.getElementById("alert").classList.add("invisible");
+
+    let userString = document.getElementById("userString").value;
+
+    let reversedString = reverseString(userString);
+
+    displayString(reversedString);
+}
+
+//Reverse the string
+//Logic function
+function reverseString(_userString)
+{
+    let reversedString = [];
+
+    for(let index = _userString.length - 1; index >= 0; index--)
+    {
+        reversedString += _userString[index];
+    }
+
+    return reversedString;
+}
+
+//Display the reversed string to the user
+//View function 
+function displayString(_reversedString)
+{
+    //Write to thje page 
+    document.getElementById("message").innerHTML =  `Your string reversed is: ${_reversedString}`;
+    // Show the alert box
+    document.getElementById("alert").classList.remove("invisible");
 }
